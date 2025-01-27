5db71200a5c7d7df689dde2dec0bf57274bec09e
alonso trina <alonsotrina22@gmail.com>
2025-01-27 09:13:39 -0300

    subiendo cambios módulo de Reportes diarios, y modifcaciones en los filtros

--- assets/js/filter-incidemtes.js
+++ assets/js/filter-incidemtes.js
@@ -1,20 +1,57 @@
-// Form incidentes
-const filterFormIncidents = document.getElementById('filterFormIncidents');
-const clearFilterBtnIncidents = document.getElementById('clearFilterIncidents');
-const searchBtnIncidents = document.getElementById('search-incidents');
+function handleFilters(formId, clearBtnId, searchBtnId, fieldsToReset = []) {
+  const form = document.getElementById(formId);
+  const clearBtn = document.getElementById(clearBtnId);
+  const searchBtn = document.getElementById(searchBtnId);
+
+  if (!form || !clearBtn || !searchBtn) {
+    console.error(`No se encontraron uno o más elementos: ${formId}, ${clearBtnId}, ${searchBtnId}`);
+    return;
+  }
 
 
-console.log('filterFormIncidents', filterFormIncidents)
+  // Evento al enviar el formulario
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    clearBtn.classList.remove('display-none-button');
+    searchBtn.classList.add('display-none-button');
+  });
 
-filterFormIncidents.addEventListener('submit', function (event) {
-  event.preventDefault();
-  clearFilterBtnIncidents.classList.remove('display-none-button');
-  searchBtnIncidents.classList.add('display-none-button')
-});
+  // Evento para limpiar los filtros
+  clearBtn.addEventListener('click', function () {
+    fieldsToReset.forEach((fieldId) => {
+      const field = document.getElementById(fieldId);
+      if (field.tagName === 'SELECT') {
+        field.selectedIndex = 0; // Resetear selects
+      } else if (field.tagName === 'INPUT') {
+        field.value = ''; // Resetear inputs
+      }
+    });
 
-// Limpiar filtro
-clearFilterBtnIncidents.addEventListener('click', function () {
-  document.getElementById('type-error-incidents').selectedIndex = 0;
-  clearFilterBtnIncidents.classList.add('display-none-button');
-  searchBtnIncidents.classList.remove('display-none-button')
-});
+    clearBtn.classList.add('display-none-button');
+    searchBtn.classList.remove('display-none-button');
+  });
+}
+// Form reporte descarga
+handleFilters('filterFormDownloadReport', 'clearFilterDownloadReport', 'searchDownloadReport', [
+  'date-download-report',
+  'input-download-report',
+]);
+
+// Form reporte bitácora
+handleFilters('filterFormLogReport', 'clearFilterLogReport', 'searchLogReport', [
+  'date-log-report',
+  'user-download-report',
+  'download-report',
+]);
+
+// Form monitoreo incidnetes
+handleFilters('filterFormIncidents', 'clearFilterIncidents', 'searchIncidents', [
+  'select-incidents'
+]);
+
+// Form monitoreo histórioc
+handleFilters('filterFormHistory', 'clearFilterHistory', 'searchHistory', [
+  'tipo-error',
+  'datepickerHistoyFrom',
+  'datepickerHistoyTo',
+]);
--- assets/js/pikaday.js
+++ assets/js/pikaday.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
   new Pikaday({
-    field: document.getElementById('datepicker'),
+    field: document.getElementById('datepickerHistoyFrom'),
     theme: "pikaday-white",
     format: 'DD/MM/YYYY',
     toString(date, format) {
@@ -49,7 +49,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   });
   new Pikaday({
-    field: document.getElementById('datepicker2'),
+    field: document.getElementById('datepickerHistoyTo'),
     theme: "pikaday-white",
     format: 'DD/MM/YYYY',
     toString(date, format) {
@@ -97,28 +97,109 @@ window.addEventListener('DOMContentLoaded', () => {
       weekdaysShort: ['D', 'L', 'M', 'M', 'J', 'V', 'S']
     }
   });
+  
+  const pcikerDownloadReport= new Pikaday({
+    field: document.getElementById('date-download-report'),
+    theme: "pikaday-white",
+    format: 'DD/MM/YYYY',
+    firstDay: 1,
+    toString(date, format) {
+      const day = String(date.getDate()).padStart(2, '0');
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const year = date.getFullYear();
+      return `${day}/${month}/${year}`;
+    },
+    parse(dateString, format) {
+      const parts = dateString.split('/');
+      const day = parseInt(parts[0], 10);
+      const month = parseInt(parts[1], 10) - 1;
+      const year = parseInt(parts[2], 10);
+      return new Date(year, month, day);
+    },
+    onSelect: function (date) {
+      console.log(date);
+    },
+    i18n: {
+      previousMonth: 'Prev',
+      nextMonth: 'Next',
+      months: [
+        'Ene.',
+        'Feb.',
+        'Mar.',
+        'Abr.',
+        'May.',
+        'Jun.',
+        'Jul.',
+        'Aug.',
+        'Sept.',
+        'Oct.',
+        'Nov.',
+        'Dic.'
+      ],
+      weekdays: [
+        'Domingo',
+        'Lunes',
+        'Martes',
+        'Miércoles',
+        'Jueves',
+        'Viernes',
+        'Sábado',
+      ],
+      weekdaysShort: ['D', 'L', 'M', 'M', 'J', 'V', 'S']
+    }
+  });
+  pcikerDownloadReport.setDate(new Date());
+
+  const pcikerLogReport= new Pikaday({
+    field: document.getElementById('date-log-report'),
+    theme: "pikaday-white",
+    format: 'DD/MM/YYYY',
+    firstDay: 1,
+    toString(date, format) {
+      const day = String(date.getDate()).padStart(2, '0');
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const year = date.getFullYear();
+      return `${day}/${month}/${year}`;
+    },
+    parse(dateString, format) {
+      const parts = dateString.split('/');
+      const day = parseInt(parts[0], 10);
+      const month = parseInt(parts[1], 10) - 1;
+      const year = parseInt(parts[2], 10);
+      return new Date(year, month, day);
+    },
+    onSelect: function (date) {
+      console.log(date);
+    },
+    i18n: {
+      previousMonth: 'Prev',
+      nextMonth: 'Next',
+      months: [
+        'Ene.',
+        'Feb.',
+        'Mar.',
+        'Abr.',
+        'May.',
+        'Jun.',
+        'Jul.',
+        'Aug.',
+        'Sept.',
+        'Oct.',
+        'Nov.',
+        'Dic.'
+      ],
+      weekdays: [
+        'Domingo',
+        'Lunes',
+        'Martes',
+        'Miércoles',
+        'Jueves',
+        'Viernes',
+        'Sábado',
+      ],
+      weekdaysShort: ['D', 'L', 'M', 'M', 'J', 'V', 'S']
+    }
+  });
+  pcikerLogReport.setDate(new Date());
 })
 
-
-
-// Limpiar filtros
-// Form historico
-const filterForm = document.getElementById('filterForm');
-const clearFilterBtn = document.getElementById('clearFilter');
-const searchBtn = document.getElementById('search');
-
-
-filterForm.addEventListener('submit', function (event) {
-  event.preventDefault();
-  clearFilterBtn.classList.remove('display-none-button');
-  searchBtn.classList.add('display-none-button')
-});
-
-// Limpiar filtro
-clearFilterBtn.addEventListener('click', function () {
-  document.getElementById('tipo-error').selectedIndex = 0;
-  document.getElementById('datepicker').value = '';
-  document.getElementById('datepicker2').value = '';
-  clearFilterBtn.classList.add('display-none-button');
-  searchBtn.classList.remove('display-none-button')
-});
